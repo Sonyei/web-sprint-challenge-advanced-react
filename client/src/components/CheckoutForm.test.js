234fe25c4b6dfaac8be33d0908b54872202c3421
Sonyei <esonyei@gmail.com>
2020-12-04 12:43:44 -0500

    imports are important

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { screen, getByLabelText, render } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -10,5 +11,25 @@ test("form header renders", () => {
 });
 
 test("form shows success message on submit with form details", () => {
+
+    render(<CheckoutForm />)
     
+        //Act
+
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/last name/i)
+
+    userEvent.type(firstNameInput, 'Angel')
+    userEvent.type(lastNameInput, 'AngelLN')
+
+
+    const button = screen.getByRole("button")
+    userEvent.click(button);
+
+
+        //Assert
+
+    // expect(screen.getByText(/Angel/i).toBeinTheDocument();
+    // expect(screen.getByText(/AngelLN/i).toBeinTheDocument();
+
 });
