d9752e677484a3811147eb883d93ab13d6024823
Sonyei <esonyei@gmail.com>
2020-12-04 12:49:09 -0500

    Intellisense trying to mess me up

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -18,10 +18,17 @@ test("form shows success message on submit with form details", () => {
 
     const firstNameInput = screen.getByLabelText(/first name/i)
     const lastNameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
 
     userEvent.type(firstNameInput, 'Angel')
     userEvent.type(lastNameInput, 'AngelLN')
-
+    userEvent.type(addressInput, 'Angel Street 404')
+    userEvent.type(cityInput, 'Cityvilleton')
+    userEvent.type(stateInput, 'AV')
+    userEvent.type(zipInput, '40404')
 
     const button = screen.getByRole("button")
     userEvent.click(button);
