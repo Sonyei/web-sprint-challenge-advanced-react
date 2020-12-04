0611e929e0b4a6e70f704d4ceb91e99aec73e882
Sonyei <esonyei@gmail.com>
2020-12-04 11:41:37 -0500

    API call and data rendering

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,15 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+// add state with a property called "plants" - initialize as an empty array
+constructor() {
+  super()
+    this.state = {
+    plants: []
+  }
+}
 
-  // when the component mounts:
+ // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+    .then(res => {
+      // console.log(res.data)
+      const resData = res.data.plantsData
+      this.setState({ plants: resData })
+    })
+    .catch(err => {
+    (console.log(err))
+  })
+
+}
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
+
     return (
       <main className="plant-list">
         {this.state?.plants?.map((plant) => (
