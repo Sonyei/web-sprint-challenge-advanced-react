e5541cdf68891d79e605f74a8f253f874ccbc2b7
Angelika Blackburn <esonyei@gmail.com>
2021-01-15 13:10:29 -0500

    darkmode styling tweaks finished

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,10 +1,6 @@
-// write your custom hook here to control your checkout form
-// stop importing React for no reason T.T
-
 import { useState } from 'react'
 
 export const useForm = (initialValue) => {
-
     const [value, setValue] = useState(initialValue)
 
     const handleChanges = e => {
@@ -13,8 +9,11 @@ export const useForm = (initialValue) => {
         [e.target.name]: e.target.value
         })
     }
-
     return (
         [value, handleChanges]
     )
 }
+
+
+// write your custom hook here to control your checkout form
+// stop importing React for no reason T.T
